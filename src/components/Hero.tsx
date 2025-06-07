d0551962fdf62b08cf421d9f77496f7e8ebd6644
yuiseki <yuiseki@users.noreply.github.com>
2025-06-07 01:43:59 +0000

    deploy: 7b21e057d6b1a966e40e4f3ef977ae0235bee1d6

--- src/components/Hero.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Hero() {
-  return (
-    <section id="hero">
-      <h1>🇺🇳 UNVT Dashboard 🗺️</h1>
-      <img src="https://un-vector-tile-toolkit.github.io/signature/logo.png" alt="UNVT logo" />
-    </section>
-  );
-}
